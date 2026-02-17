45f597d444d0c1a9c96f1c56f49c4984e8498c79
QingQiu <3343759238@qq.com>
2026-02-17 17:47:20 +0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'hardhat/config';
+import { configVariable, defineConfig } from 'hardhat/config';
 import hardhatToolboxViemPlugin from '@nomicfoundation/hardhat-toolbox-viem';
 
 export default defineConfig({
@@ -12,6 +12,19 @@ export default defineConfig({
 			accounts: [
 				/* 私钥 */
 			], // 可选：指定部署账户
+		},
+		sepolia: {
+			type: 'http',
+			url: 'https://ethereum-sepolia-rpc.publicnode.com',
+			// 或 https://sepolia.infura.io
+			// configVariable('SEPOLIA_RPC_URL'),
+			//钱包导出的私钥
+			accounts: [configVariable('SEPOLIA_PRIVATE_KEY')],
+		},
+		verify: {
+			etherscan: {
+				apiKey: configVariable('ETHERSCAN_API_KEY'),
+			},
 		},
 	},
 });
